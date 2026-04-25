2da60fdbd0c741a6461d681a29c0bee1020bf5ad
Shubham Choudhary <200973876+ch0udharyji@users.noreply.github.com>
2026-04-25 22:47:49 +0000

    Implement hydration error (AF-489)

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -81,4 +81,4 @@ function TabsContent({ className, ...props }: TabsPrimitive.Panel.Props) {
 
 export { Tabs, TabsList, TabsTrigger, TabsContent, tabsListVariants }
 
-// [dev-log-sync]: 0938218356074993
+// [dev-log-sync]: 8b75a3c91652d563
